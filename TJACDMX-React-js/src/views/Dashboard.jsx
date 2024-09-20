af6d5ad4a014ece83f83924546eba52c18e13c85
cuauhvip07 <cuauvillalba1@gmail.com>
2024-09-20 15:28:15 -0600

    Resideño dashboard
    SE hace un diseño mas bonito para el dashboard

--- TJACDMX-React-js/src/views/Dashboard.jsx
+++ TJACDMX-React-js/src/views/Dashboard.jsx
@@ -4,18 +4,23 @@ import { Link } from "react-router-dom"
 
 export default function Dashboard() {
   return (
-    <div>
-      <div className="flex flex-col items-center">
+    <div className=" mt-5 ml-5">
+      <div className=" grid grid-cols-3 gap-5">
 
-        <div className=" bg-yellow-400 w-7/12 text-center mt-10 py-2 font-bold ">
+        <div className=" bg-yellow-400 p-3 rounded-xl">
           <Link
             to='/magistrados'
           >
-            <div className="">
-              <p>Magistrados</p>
+            <div className=" h-48 flex items-center justify-between">
+              <p className=" font-bold uppercase text-xl">Magistrados</p>
+              <div>
+                <img src="src/img/magistrado.png" alt="Imagen magistrado" className=" size-20"/>
+              </div>
             </div>
           </Link>
         </div>
+
+        
 
       </div>
     </div>
